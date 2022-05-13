c27c0acecf946ef96adc82a312c3dd4b32addefb
Anna <annhayriyan@gmail.com>
2022-05-13 04:23:39 -0400

    algo parecido a lo que queria

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,57 +1,55 @@
 import "./Popup.css";
 import {useEffect, useState} from "react";
-import {IoMdResize} from "react-icons/all";
+import {IoMdResize} from "react-icons/io";
 function Popup({active, close,popUpToButton}){
 
-    const sizes = ["small","medium", "large"];
-    let i = 0;
+   // const sizes = ["small","medium", "large"];
 
-    const [className, setClassName] = useState("modal");
+    const [className, setClassName] = useState("modal")
 
-    function addClassName(e){
-        console.log(e.target.name)
-        setClassName(`modal ${e.target.name}`)
+    // eslint-disable-next-line no-restricted-globals
+    function addClassName(current = event.target.name){
+        console.log(current)
+        setClassName(`modal ${current}`)
     }
-
     function changeClassName(){
         return className;
     }
 
-    function changeTheSize(){
+    useEffect(()=>{
+        changeClassName();
+        },[className]
 
-        console.log(sizes[i-1])
-        console.log(i)
-        setClassName(`modal ${sizes[i]}`);
-        console.log(className)
-        i++;
-        console.log(i)
-    }
+    )
 
-    // useEffect(()=>{
-    //     changeClassName();
-    //     },[className]
+    //    let i = 0;
+    // function changeTheSize(){
+    //     if(i === 4 ){
+    //         i=0;
+    //     }
+    //     i++;
+    //     console.log()
+    //     return sizes[i-1];
     //
-    // )
-
-
-
-
-
+    // }
 
     if(!active){
         return null;
     }
 
     return(
-
-    <div className={changeClassName()}>
-
-        <div className="modal-content">
+        <div className="backdrop" onClick={ ()=>
+            {close();
+            popUpToButton();
+            addClassName("modal")}
+        }>
+        <div className={changeClassName()}>
+        {/*<div className="modal-content">*/}
             <div className="modal-header">
-                <IoMdResize className="resizeIcon" onClick={changeTheSize}></IoMdResize>
-              {/*<button onClick={addClassName} name="small">Small</button>*/}
-              {/*  <button onClick={addClassName} name="medium">Medium</button>*/}
-              {/*  <button onClick={addClassName} name="large">Large</button>*/}
+                {/*<IoMdResize className="resizeIcon" onClick={changeTheSize}></IoMdResize>*/}
+                <button onClick={()=>addClassName()} name="small" className="sizeButton">S</button>
+                <button onClick={()=>addClassName()} name="medium"className="sizeButton">M</button>
+                <button onClick={()=>addClassName()} name="large"className="sizeButton">L</button>
             </div>
             <div className="modal-body">
                 aqui esta el contenido
@@ -61,19 +59,22 @@ function Popup({active, close,popUpToButton}){
                 <button className="buttonConfirm" onClick={() => {
                     close();
                     popUpToButton();
+                    addClassName("modal")
                 }}>
                     Confirm
                 </button>
                 <button className="buttonClose" onClick={()=> {
                     close();
-                    popUpToButton()
+                    popUpToButton();
+                    addClassName("modal")
                 }
                 }>
                     Close
                 </button>
             </div>
-        </div>
+        {/*</div>*/}
     </div>
+        </div>
 
     )
 
